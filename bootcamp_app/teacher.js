94f00b9601d4053f0d3fe1d799c8c2f9d5e0ef5a
Monica Li <monicali@live.ca>
2020-11-26 07:25:26 +0000

    adds parametized queries to teachers.js and students.js

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -6,16 +6,16 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
+const cohortName = process.argv[2];
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, [cohortName])
 .then(res => {
     let i = 0;
     // console.log(`${res.rows.cohort}: ${res.rows.teacher}`);
